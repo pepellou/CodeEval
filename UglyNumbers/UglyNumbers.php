08e175f8ce567c9081a57b62b451abfd3248475f
Pepe Doval <pepellou@gmail.com>
2013-10-11 03:02:56 +0200

    UglyNumbers solution

---
+++ UglyNumbers/UglyNumbers.php
@@ -0,0 +1,150 @@
+<?php
+
+class Condition {
+
+    private $value;
+
+    public function __construct(
+        $value
+    ) {
+        $this->value = $value;
+    }
+
+    public function ugly(
+    ) {
+        foreach (array(2, 3, 5, 7) as $uglifier) {
+            if (Is($this->value)->multipleOf($uglifier)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function multipleOf(
+        $anotherNumber
+    ) {
+        return ($this->value % $anotherNumber == 0);
+    }
+
+}
+
+function Is($what) {
+    return new Condition($what);
+}
+
+class State {
+
+    public $toSolve;
+    public $currentResult = 0;
+    public $currentOperator = "";
+    public $currentSign = 1;
+
+    public function __construct(
+        $toSolve,
+        $currentResult = 0,
+        $currentOperator = "",
+        $currentSign = 1
+    ) {
+        $this->toSolve = $toSolve;
+        $this->currentResult = $currentResult;
+        $this->currentOperator = $currentOperator;
+        $this->currentSign = $currentSign;
+    }
+
+    public function noop(
+    ) {
+        return new State(
+            $this->restToSolve(),
+            $this->currentResult, 
+            $this->currentOperator.$this->nextDigit(),
+            $this->currentSign
+        );
+    }
+
+    public function plus(
+    ) {
+        return new State(
+            $this->restToSolve(),
+            $this->updatedResult(),
+            "", 
+            1
+        );
+    }
+
+    public function minus(
+    ) {
+        return new State(
+            $this->restToSolve(),
+            $this->updatedResult(),
+            "",
+            -1
+        );
+    }
+
+    private function restToSolve(
+    ) {
+        return substr($this->toSolve, 1);
+    }
+
+    public function updatedResult(
+    ) {
+        return $this->currentResult + $this->currentSign * ($this->currentOperator . $this->nextDigit());
+    }
+
+    private function nextDigit(
+    ) {
+        return $this->toSolve[0];
+    }
+
+    public function isFinal(
+    ) {
+        return $this->restToSolve() == "";
+    }
+
+}
+
+class UglyCounter {
+
+    public $count = 0;
+
+    public function check(
+        $number
+    ) {
+        if (Is($number)->ugly()) {
+            $this->count++;
+        }
+    }
+
+}
+
+class StateExplorer {
+
+    private $counter;
+
+    public function __construct(
+        $counter
+    ) {
+        $this->counter = $counter;
+    }    
+
+    public function explore(
+        $state
+    ) {
+        if ($state->isFinal()) {
+            $this->counter->check($state->updatedResult());
+            return;
+        }
+        $this->explore($state->noop());
+        $this->explore($state->plus());
+        $this->explore($state->minus());
+    }
+
+}
+
+
+foreach (file($argv[1]) as $digits) {
+    $counter = new UglyCounter();
+    $solver = new StateExplorer($counter);
+    $solver->explore(new State(trim($digits)));
+    echo $counter->count."\n";
+}
